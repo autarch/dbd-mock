cc183d6ced76a150b1bc656516e51cf4a95cdce4
bluescreen10 <dichoso@gmail.com>
2011-06-22 14:16:19 -0300

    Version 1.41

--- lib/DBD/Mock.pm
+++ lib/DBD/Mock.pm
@@ -20,7 +20,7 @@ use warnings;
 
 require DBI;
 
-our $VERSION = '1.40';
+our $VERSION = '1.41';
 
 our $drh    = undef;    # will hold driver handle
 our $err    = 0;        # will hold any error codes
